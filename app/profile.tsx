e175d503a1cf80c629718540cad4cc4c0b5edefa
keerthivasan2002 <skeerthi2002@gmail.com>
2025-03-19 16:54:01 +0800

    test test

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
-    <View>
+      <View className='flex-1 bg-primary-50'>
       <Text>Profile</Text>
     </View>
+    
   )
 }
 
