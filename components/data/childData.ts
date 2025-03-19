b3d3f023e64eb17614386f5acf7cc80afe149dd2
keerthivasan2002 <skeerthi2002@gmail.com>
2025-03-20 00:08:52 +0800

    profile almost done

---
+++ components/data/childData.ts
@@ -0,0 +1,9 @@
+export const child = {
+    name: "Jaslyn",
+    school: "Isaac Predatory Primary School",
+    classInfo: "1E4",
+    grade: "P1",
+    attendanceStatus: "Absent",
+    profileImage: require("../../assets/images/legoat.jpg"),
+  };
+  
